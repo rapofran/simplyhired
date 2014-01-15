f52a17045e5cc145d5b2a7cb3352da5eef70edfe
murty korada <murtyk@yahoo.com>
2014-01-15 15:31:44 -0500

    title encode

--- lib/simplyhired/job.rb
+++ lib/simplyhired/job.rb
@@ -2,16 +2,18 @@ module Simplyhired
 	class Job
 		attr_reader :title, :company, :date_posted, :excerpt, :location, :source, :details_url, :details
 		def initialize(jhash)
-			@title = jhash[:title]
+			encoding_options = {
+				invalid: :replace,		# Replace invalid byte sequences
+				undef: :replace,		# Replace anything not defined in ASCII
+				replace:  '',			# Use a blank for those replacements
+				universal_newline: true # Always break lines with \n
+			}
+			if jhash[:title]
+				@title = jhash[:title].gsub("&amp;", "&")
+			end
 			@excerpt = jhash[:excerpt]
 			@location = jhash[:location]
 			if jhash[:company]
-				  encoding_options = {
-				    invalid: :replace,		# Replace invalid byte sequences
-				    undef: :replace,		# Replace anything not defined in ASCII
-				    replace:  '',			# Use a blank for those replacements
-				    universal_newline: true # Always break lines with \n
-				  }
 				name = jhash[:company].encode Encoding.find('ASCII'), encoding_options
 				@company = name.gsub("&amp;", "&")
 			end
